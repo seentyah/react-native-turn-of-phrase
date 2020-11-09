aa5e44b01a97bc9cf4f5051838030c1b46757e70
seentyah <66042493+seentyah@users.noreply.github.com>
2020-11-08 21:49:07 -0600

    Add files via upload

---
+++ prompts.js
@@ -0,0 +1,33 @@
+export const PROMPTS = [
+    
+    {
+        id: 0,
+        image: "../assets/foodforthought.gif",
+        userAnswer: [
+            { answerText: 'Food for thought', isCorrect:true },
+            { answerText: 'Mind your plate', isCorrect: false},
+        ] ,  
+    },
+
+    {
+        id: 1,
+        image: "../assets/curiosity.gif",
+        
+        userAnswer: [
+            { answerText: 'Curiosity killed the cat', isCorrect:true },
+            { answerText: 'Cat has nine lives', isCorrect: false}
+
+        ],
+    },
+
+    {
+        id: 2,
+        image: "../assets/talldrink.gif",
+        userAnswer: [
+            { answerText: ' Tall drink of water', isCorrect:true },
+            { answerText: 'Fresh drink of water', isCorrect: false}
+        ],
+        
+    }
+
+];
